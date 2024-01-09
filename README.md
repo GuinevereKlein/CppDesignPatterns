46b8041f48df87cb9862616d1708c25dd498f52b
andrewklein <andrew.klein@daaci.com>
2024-01-09 14:28:47 +0000

    variables and readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CppDesignPatterns
 This is a project I'm using to improve my knowledge of C++ and design patterns
+
+## Tutorials used
+[Learn modern Cpp](https://learnmoderncpp.com/)
--- learning_variables/variables.cpp
+++ learning_variables/variables.cpp
@@ -6,7 +6,7 @@ int main(){
     int a=1;
     double b=2.5;
     cout << "a="<<a<<" b="<<b<<endl;
-    a=b;
-    b=1;
+    a=static_cast<int>(2.5);
+    b=static_cast<double>(1);
     cout << "a="<<a<<" b="<<b<<endl;
 }
